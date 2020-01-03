aba624c8d814723200e2b7fd3b1bdb499f8d7754
stelthdroid8 <brandon.kennedy1994@gmail.com>
2020-01-02 22:17:57 -0500

    add basic start and pause timer functionality

---
+++ index.js
@@ -0,0 +1,42 @@
+class Timer {
+    constructor(durationInput, startButton, pauseButton) {
+        this.durationInput = durationInput;
+        this.startButton = startButton;
+        this.pauseButton = pauseButton;
+    
+        this.startButton.addEventListener('click', this.start);
+        this.pauseButton.addEventListener('click', this.pause);
+    
+    }
+
+    start = () => {
+        this.tick();
+        this.intervalID = setInterval(this.tick, 1000);
+    }
+    tick = () => {
+        console.log(this.timeRemaining);
+        if (this.timeRemaining <= 0){
+            this.pause();
+        }
+        else{
+            this.timeRemaining = this.timeRemaining - 1;
+        }
+    }
+    pause = () =>{
+        clearInterval(this.intervalID);
+        console.log('paused the timer');
+    }
+
+    get timeRemaining() {
+        return parseFloat(this.durationInput.value);
+    }
+    set timeRemaining(time) {
+        this.durationInput.value = time;
+    }
+}
+
+const durationInput = document.querySelector('#duration');
+const startButton = document.querySelector('#start');
+const pauseButton = document.querySelector('#pause');
+
+const timer = new Timer(durationInput, startButton, pauseButton);
