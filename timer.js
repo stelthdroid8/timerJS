9bc842a3744faf819aff43bd87c13b009f74184b
stelthdroid8 <brandon.kennedy1994@gmail.com>
2020-01-03 14:58:55 -0500

    refactor timer and add support for border

---
+++ timer.js
@@ -0,0 +1,51 @@
+class Timer {
+    constructor(durationInput, startButton, pauseButton, callbacks) {
+        this.durationInput = durationInput;
+        this.startButton = startButton;
+        this.pauseButton = pauseButton;
+        if (callbacks) {
+            this.onStart = callbacks.onStart;
+            this.onTick = callbacks.onTick;
+            this.onComplete = callbacks.onComplete;
+        }
+
+        this.startButton.addEventListener('click', this.start);
+        this.pauseButton.addEventListener('click', this.pause);
+
+    }
+
+    start = () => {
+        if (this.onStart) {
+            this.onStart(this.timeRemaining);
+        }
+        this.tick();
+        this.intervalID = setInterval(this.tick, 50);
+    }
+    tick = () => {
+        // console.log(this.timeRemaining);
+
+        if (this.timeRemaining <= 0) {
+            this.pause();
+            if (this.onComplete) {
+                this.onComplete();
+            }
+        }
+        else {
+            this.timeRemaining = this.timeRemaining - .05;
+            if (this.onTick) {
+                this.onTick(this.timeRemaining);
+            }
+        }
+    }
+    pause = () => {
+        clearInterval(this.intervalID);
+        console.log('paused the timer');
+    }
+
+    get timeRemaining() {
+        return parseFloat(this.durationInput.value);
+    }
+    set timeRemaining(time) {
+        this.durationInput.value = time.toFixed(2);
+    }
+}
